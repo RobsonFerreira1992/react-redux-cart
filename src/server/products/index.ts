9fed7f32e2e666fb829700baf15d402001da7592
robson <robson1992ferreira@hotmail.com>
2023-03-10 15:02:39 -0300

    remove pasta server

--- src/server/products/index.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosResponse } from 'axios'
-import api from 'server/api';
-import { MovieProps } from 'types';
-
-export async function GetMovies(): Promise<AxiosResponse<any>>{
-  return api.get('/products')
-}
